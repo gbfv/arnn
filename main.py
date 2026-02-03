Fix bug reset gradient

main.py
@@ -84,7 +84,7 @@ def batch_rnn_build(name, training_files, batch_size=1024, whitelist=True, save=
     return model
 
 
-def batch_gru_build(name, training_files, whitelist=True, save=True):
+def batch_gru_build(name, training_files, whitelist=True, save=True, DA=-2, DB=20):
     model_path = f"models/{name}.pth"
     # Loading model
     if pathlib.Path(model_path).is_file():
@@ -92,7 +92,7 @@ def batch_gru_build(name, training_files, whitelist=True, save=True):
     
     # Building model
     dataset = LogDataset(files=training_files, whitelist=whitelist)
-    dataset.prep_batch(DA=-2, DB=20)
+    dataset.prep_batch(DA=DA, DB=DB)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1024, shuffle=True)
     print(f"Fichier chargé pour l'entraînement : {dataset.used_files}")
 
@@ -143,8 +143,7 @@ def test_model_batch(model_name, files, batch_size=1024, whitelist=True, DA=-2, 
         prec, recall, f1 = model.scores(y_t, predicted)
         print(f"Scores : {prec}, {recall}, {f1}")
         return prec, recall, f1
-
-
+    
 
 ### Automaton functions ###
 
@@ -230,7 +229,7 @@ def test_automaton(automaton_name, files, model):
     return prec, recall, f1
 
 
-def test_automaton_batch(automaton_name, files, model):
+def test_automaton_batch(automaton_name, files, model, DA=-2, DB=20):
     auto_path = f"automate/{automaton_name}.pkl"
     if pathlib.Path(auto_path).is_file():
         with open(auto_path, "rb") as f:
@@ -242,7 +241,7 @@ def test_automaton_batch(automaton_name, files, model):
 
     # Preparing test data
     dataset = LogDataset(files=files, whitelist=True)
-    dataset.prep_batch()
+    dataset.prep_batch(DA=DA, DB=DB)
     print(f"Taille d'une séquence de test : {len(dataset.data[0])}")
     y_t = torch.tensor(dataset.labels).numpy()
 
@@ -323,6 +322,31 @@ def big_run(build, test, model_name, automaton, states):
         json.dump(res, f)
 
 
+def loss_stat(model_name, build_files=["kernel32.log", "msvcr100.log", "user32.log"], test_file=["ntdll.log"], rnn=True):
+    res = []
+    for _ in range(20):
+        if rnn:
+            batch_rnn_build("btest.500", build_files, save=False)
+        else:
+            batch_gru_build("bgru_test.500", build_files, save=False)
+        
+        files = os.listdir("test/")
+        files = sorted([f for f in files if f.endswith(".pth")], key=lambda x: int(x.split(".")[0]))
+        print(files)
+
+        prf = [[], [], []]  # precision, recall, f1
+        for file in files:
+            print(f"Testing model saved at epoch {file.split('.')[0]}...")
+            prec, recall, f1 = test_model_batch(f"{file.split('.')[0]}", test_file)
+            prf[0].append(prec)
+            prf[1].append(recall)
+            prf[2].append(f1)
+            os.remove(f"test/{file}")
+        res.append(prf)
+
+    with open("model_perfs.json", "w") as f:
+        json.dump(res, f)
+
 
 def stats_states(auto_path, files, whitelist=True):
     with open(auto_path, "rb") as f:
@@ -360,6 +384,9 @@ def stats_states(auto_path, files, whitelist=True):
 
 
 
+
+
+
 ##### Main #####
 
 
@@ -367,60 +394,45 @@ if __name__ == "__main__":
     """ build = ["kernel32.log", "msvcr100.log", "user32.log", "ntdll.log", "libcrypto.log", "firewallAPI.log", "ws2_32.log", "signdrv.log", "cmdext.log", "gdi32.log"]
     test = ["kerberos.log","ieproxy.log","crypt32.log","clp64.log","energy.log","basesrv.log"]
     
-    automaton = "bgruCelica.500_"
+    automaton = "bgruCelicaV2.500_"
     states = [500, 1000, 2000, 6000, 6500, 10000]
 
-    big_run(build, test, "bgru_Celica.500", automaton, states) """
-    
+    big_run(build, test, "bgru_CelicaV2.500", automaton, states) """
 
-    """ # Loss test for batch RNN
-    res = []
-    for _ in range(20):
-        batch_rnn_build("bkmu.500", ["kernel32.log", "msvcr100.log", "user32.log"], save=False)
-        
-        files = os.listdir("test/")
-        files = sorted([f for f in files if f.endswith(".pth")], key=lambda x: int(x.split(".")[0]))
-        print(files)
 
-        prf = [[], [], []]  # precision, recall, f1
-        for file in files:
-            print(f"Testing model saved at epoch {file.split('.')[0]}...")
-            prec, recall, f1 = test_model_batch(f"{file.split('.')[0]}", ["ntdll.log"])
-            prf[0].append(prec)
-            prf[1].append(recall)
-            prf[2].append(f1)
-            os.remove(f"test/{file}")
-        res.append(prf)
-
-    with open("model_perfs.json", "w") as f:
-        json.dump(res, f) """
-    
-
-    """ stats = stats_states("automate/bgru_Celica.500_1000.pkl", [], whitelist=False)
+    """ stats = stats_states("automate/bgru_CelicaV2.500_1000.pkl", [], whitelist=False)
     
     stats = sorted(stats.items(), key=lambda x: x[1]["frequence"], reverse=True)
     for s, v in stats[:20]:
         print(f"État {s:<3} :  fréquence {v['frequence']*100:>5.1f} | count {v['count']:>5} | pos_1 : {v['1']:>5} | pos_2 : {v['2']:>5} | pos_3 : {v['3']:>5}") """
 
-    """ with open("automate/bgru_Celica.500_1000.pkl", "rb") as f:
+
+    """ with open("automate/bgru_CelicaV7.500_1000.pkl", "rb") as f:
         A = pickle.load(f)
 
-    X, y = testing_data("data/kernel32.log", DELTA=0)
-    predicted = A.predict_flow(torch.tensor(X), y)
+    X, y = testing_data("data/kerberos.log", DELTA=0)
+    predicted = A.predict_flow(torch.tensor(X), y, window_size=48)
 
     print(f"nombre de fonctions dans y : {sum(y)} / {len(y)}")
     print(f"nombre de fonctions dans predicted : {sum(predicted)} / {len(predicted)}")
     print(f'\nAutomaton  : {predicted} / True : {y[:20]}')
     print(f'Diff : {sum(abs(y - predicted))}')
     
-    model = load_model("bgru_Celica.500")
+    model = load_model("bgru_CelicaV7.500")
     prec, recall, f1 = model.scores(y, predicted)
-    print(f"Scores : Precision: {prec}, Recall: {recall}, F1-score: {f1}")
+    print(f"Scores : Precision: {prec}, Recall: {recall}, F1-score: {f1}") """
 
-    #test = [108, 0, 205, 2, 82, 116, 108, 78, 116, 80, 97, 116, 104, 78, 97, 109, 101, 84, 111, 68, 111, 115]
-    test = [224, 0, 78, 116, 81, 117, 101, 114, 121, 68, 101, 102, 97, 117, 108, 116, 76, 111, 99, 97, 108, 101]
-    pred, _ = A.predict(torch.tensor(test))
-    print(f'Automaton Prediction for sample : {pred}')
-    model = load_model("bgru_Celica.500")
-    bgru_res, _ = model.forward(torch.tensor(test).unsqueeze(0).to(DEVICE))
-    print(f'GRU Prediction for sample : {(bgru_res.squeeze() > 0.8).int().detach().cpu().numpy()}') """
+
+
+    """ model = load_model("bgru_CelicaV5.500")
+    X, y = testing_data("data/kernel32.log", DELTA=0)
+    predicted = model.predict_flow(torch.tensor(X), y, window_size=22)
+
+    print(f"nombre de fonctions dans y : {sum(y)} / {len(y)}")
+    print(f"nombre de fonctions dans predicted : {sum(predicted)} / {len(predicted)}")
+    print(f'\nAutomaton  : {predicted} / True : {y[:20]}')
+    print(f'Diff : {sum(abs(y - predicted))}')
+    
+    model = load_model("bgru_CelicaV5.500")
+    prec, recall, f1 = model.scores(y, predicted)
+    print(f"Scores : Precision: {prec}, Recall: {recall}, F1-score: {f1}") """

rnn.py
@@ -63,29 +63,41 @@ class ARNN(nn.Module):
             hidden_states.append(hidden_state)
         return torch.cat(hidden_states, dim=1)
 
+
     def train(self, dataloader):
         a = ""
         optimizer = optim.Adam(self.parameters(), lr=0.001)  # Adam optimizer
         chunk_size = 65535  # Taille des chunks pour l'entrainement
-        total_loss = 0.0
-        cpt = 0
+
         # Boucle d'entraînement
         for epoch in range(ARNN.epochs):
-            optimizer.zero_grad()  # Réinitialiser les gradients
+            total_loss = 0.0
+            cpt = 0
             for X, y, mask in dataloader:
-                X = X.to(DEVICE)
-                y = y.to(DEVICE)
-                mask = mask.to(DEVICE)
+                X, y, mask = X.to(DEVICE), y.to(DEVICE), mask.to(DEVICE)
                 num_chunks = X.size(1) // chunk_size if X.size(1) % chunk_size == 0 else X.size(1) // chunk_size + 1
                 hidden_state = None
                 outputs = None
+
                 for chunk_idx in range(num_chunks):
-                    start_idx = chunk_idx * chunk_size
-                    end_idx = min((chunk_idx + 1) * chunk_size, X.size(1))
-                    op, hidden_state = self(X[:, start_idx:end_idx], hidden_state)  # Prédiction du modèle
-                    outputs = torch.cat((outputs, op), dim=1) if outputs is not None else op
-                    #hidden_state = hidden_state.detach()  # évite la rétropropagation à travers tout les chunks (utile ?)
-                outputs = outputs.squeeze(2)
+                    optimizer.zero_grad()  # Réinitialiser les gradients
+                    if hidden_state is not None:
+                        hidden_state = hidden_state.detach()  # évite la rétropropagation à travers tout les chunks (utile pour la mémoire)
+                    start = chunk_idx * chunk_size
+                    end = min((chunk_idx + 1) * chunk_size, X.size(1))
+                    op, hidden_state = self(X[:, start:end], hidden_state)  # Prédiction du modèle
+                    #outputs = torch.cat((outputs, op), dim=1) if outputs is not None else op
+                    op = op.squeeze(2)
+                    mask_chunk = mask[:, start:end]
+                    loss_raw = ARNN.criterion(op, y[:, start:end])
+                    masked_loss = (loss_raw * mask_chunk).sum() / (mask_chunk.sum() + 1e-8)
+                    masked_loss.backward()
+                    optimizer.step()
+
+                    total_loss += masked_loss.item()
+                    cpt += 1
+
+                """ outputs = outputs.squeeze(2)
                 padded_loss = ARNN.criterion(outputs, y)  # Calcul de la perte total (padding inclus)
                 masked_loss = padded_loss * mask # applique le masque pour ignorer les paddings
                 loss = masked_loss.sum() / mask.sum()  # Moyenne sur les éléments non padding
@@ -93,14 +105,15 @@ class ARNN(nn.Module):
                 optimizer.step()  # Mise à jour des poids
 
                 total_loss += loss.item()
-                cpt += 1
+                cpt += 1 """
             if (epoch + 1) % 10 == 0:
-                avg_loss = total_loss / cpt
-                print(f'Epoch [{epoch+1}/{ARNN.epochs}], Loss: {avg_loss:.4f}')
-            """ avg_loss = total_loss / cpt
-            a += f"{avg_loss};"
+                print(f'Epoch [{epoch+1}/{ARNN.epochs}], Loss: {total_loss/cpt:.4f}')
+            """ a += f"{total_loss / cpt};"
+            
         with open("loss_rnn.csv", "a") as f:
             f.write(a + "\n") """
+
+
 
     def scores(self, y_true, y_pred):
         combined = list(zip(y_pred, y_true))

rnn_batch.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+
 from utils import parse_log_file
 import pathlib
 from automaton import Automaton
@@ -30,9 +32,9 @@ class BRNN(nn.Module):
 
     def forward(self, x, hidden_state=None):
         embedded = self.embedding(x)
-        _, hidden_state = self.rnn(embedded, hidden_state)
-        output = torch.sigmoid(self.linear(hidden_state))  # Prendre la dernière sortie de la séquence (many-to-one)
-        return output, hidden_state
+        _, hs = self.rnn(embedded, hidden_state)
+        output = torch.sigmoid(self.linear(hs))  # Prendre la dernière sortie de la séquence (many-to-one)
+        return output, hs
 
     def get_default_hidden_state(self):
         return torch.zeros(1, 1, self.rnn.hidden_size).to(DEVICE)
@@ -125,6 +127,35 @@ class BRNN(nn.Module):
                 op = op.squeeze()
                 outputs = torch.cat((outputs, op), dim=0) if outputs is not None else op
         return outputs #raw output
+
+
+    def predict_flow(self, X, y_true, window_size=22):
+        with torch.no_grad():
+            Xt = X.detach().cpu().numpy()
+            y = np.zeros_like(Xt)
+            starting_index = 0 # starting index of the window
+
+            hallucinations = 0
+            hidden_state = None
+
+            while starting_index + window_size <= len(Xt):
+                sample = Xt[starting_index:starting_index + window_size]
+                pred, hs = self.forward(torch.tensor(sample).unsqueeze(0).to(DEVICE), hidden_state)
+                if pred.item() > 0.8: # function found
+                    if sum(y_true[starting_index:starting_index + window_size]) == 0: #hallucination check
+                        hallucinations += 1
+                    y[starting_index + 2] = 1
+                    starting_index += 22 # jump to next window
+                    hidden_state = None
+                else:
+                    starting_index += 1 # slide the window by 1
+                    hidden_state = hs
+            print(f"Total hallucinations: {hallucinations}")
+            return y
+
+
+
+
 
 
 def prepared_data(files, DA, DB, DELTA):