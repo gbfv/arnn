b0e2357366f0e007525e06388b58a19e45974fcb
tritri <tristan.lemoine2004@gmail.com>
2026-06-22 16:01:28 +0200

    added a LOTS of graphes

--- graphs.py
+++ graphs.py
@@ -75,12 +75,55 @@ def perf_graph(filename):
     pass
     plt.cla()
     data = load_log_file_for_benchmark(filename)
-    Z = [int(getattr(x,"LEN_WORDS")) for x in data]
+    Z = [int(getattr(x,"NB_WORDS")) for x in data]
     F = [float(getattr(x,"F1_M")) for x in data]
     FA = [float(getattr(x,"F1_A")) for x in data]
-    plt.scatter(Z,F)
-    plt.scatter(Z,FA)
+    plt.scatter(Z,F,label="F1 score modele")
+    plt.scatter(Z,FA,label="F1 score automate")
+    plt.legend()
+    plt.xlabel("Nombre de mot dans le motif")
+    plt.ylabel("Score F1")
+    plt.title("F1 scores en fonction du nombre de mots dans l'automate")
     plt.show()
 
+
+
+def weight_graph(filename):
+    plt.cla()
+    data = load_log_file_for_benchmark(filename)
+    ids = []
+    for d in data:
+        id_tmp = getattr(d,"ID")
+        if id_tmp not in ids:
+            ids.append(id_tmp)
+    for_graph = [x for x in range(len(ids))]
+    f1_0 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 0 ]
+    f1_1 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 1 ]
+    f1_2 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 2 ]
+    f1_3 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 3 ]
+    f1_4 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 4 ]
+    f1_5 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 5 ]
+    f1_6 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 6 ]
+    f1_7 = [float(getattr(x,"F1_M"))for x in data if int(getattr(x,"WEIGHTS")) == 7 ]
+    F1s = [f1_0,f1_1,f1_2,f1_3,f1_4,f1_5,f1_6,f1_7]
+    colors = ['b','g','r','c','m','y',"k","navy"]
+    isos = [0] * 8
+    for i in range(len(isos)):
+        isos[i] = len([1 for x in data if (int(getattr(x,"WEIGHTS")) == i and getattr(x,"ISO") == "True") ])
+    for f1_score_i in range(len(F1s)):
+        plt.hlines(np.mean(F1s[f1_score_i]),0,100,colors=colors[f1_score_i],label=f"moyenne weight {f1_score_i}, value:{np.mean(F1s[f1_score_i]):1.4}")
+    plt.scatter(for_graph,f1_0,c=colors[0],label="Weight 0")
+    plt.scatter(for_graph,f1_1,c=colors[1],label="Weight 1")
+    plt.scatter(for_graph,f1_2,c=colors[2],label="Weight 2")
+    plt.scatter(for_graph,f1_3,c=colors[3],label="Weight 3")
+    plt.scatter(for_graph,f1_4,c=colors[4],label="Weight 4")
+    plt.scatter(for_graph,f1_5,c=colors[5],label="Weight 5")
+    plt.scatter(for_graph,f1_6,c=colors[6],label="Weight 6")
+    plt.scatter(for_graph,f1_7,c=colors[7],label="Weight 7")
+    plt.xticks([])
+    plt.ylabel("F1 scrore")
+    plt.title("F1 score de modèles par différentes fonctions de poids")
+    plt.legend()
+    plt.show()
 if __name__ == "__main__":
     perf_graph("benchmark_data/perf_test_logs/logs/default/perf_log.log")
